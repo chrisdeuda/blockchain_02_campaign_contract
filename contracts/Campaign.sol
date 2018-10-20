5d6f5b9316efc2cfc10d1fa3c133df3e539e1cbd
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-20 17:33:31 +0800

    Add contract variables

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,6 +3,22 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount; //it will not be count the number of vote
+        mapping(address => bool) approvals;
+    }
+    
+    uint public approversCount;
+    
+    
+    Request[] public requests;
     
     
 }
