95553ef64b091cb3c2df5c51ef20b77d30917606
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-20 17:37:04 +0800

    Add create request method in Campaign contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -37,5 +37,17 @@ contract Campaign {
         approversCount++;
     }
     
+    function createRequest(string description, uint value, address recipient)
+        public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+    
     
 }
