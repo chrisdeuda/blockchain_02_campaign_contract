4dc68d14d4b712fe31beadf197b57de58bc7cc63
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-23 21:07:17 +0800

    Refactor test for Campaigns

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const ganace = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(ganace.provider());
+const assert    = require('assert');
+const ganace    = require('ganache-cli');
+const Web3      = require('web3');
+const web3      = new Web3(ganace.provider());
 
-const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-const compiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory   = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign  = require('../ethereum/build/Campaign.json');
 
 let accounts;
 //Referrence to the factory
@@ -13,23 +13,25 @@ let campaignAddress;
 let campaign;
 
 beforeEach( async()=> {
-    accounts = await web3.eth.getAccounts();
-
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    accounts    = await web3.eth.getAccounts();
+    factory     = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({data: compiledFactory.bytecode})
         .send({from: accounts[0], gas: '1000000'});
 
     await factory.methods.createCampaign('100').send({
         from: accounts[0],
         gas: '1000000'
-
     });
 
     // Get access the campaign that have been created from the factory
 
-    //[campaignAddress]  -> destructuring the data
-    const addressess = await factory.methods.getDeployedContracts().call();
-    campaignAddress = addressess[0];
+    /**
+     * Alternative coding
+     * [campaignAddress] = await factory.methods.getDeployedContracts().call();
+     *              It uses the ES6 Destructuring method
+     */
+    const addressess    = await factory.methods.getDeployedContracts().call();
+    campaignAddress     = addressess[0];
 
     // Using the already use the deployed contract
     campaign = await new web3.eth.Contract(
