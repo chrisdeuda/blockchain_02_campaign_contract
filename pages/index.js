01ff3f0317597c558e7e54011b834331892ea605
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-27 21:09:23 +0800

    Add button from semantic-ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
 import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
   // It skips the intial rendering without accessing the whole methods
@@ -23,14 +24,17 @@ class CampaignIndex extends Component {
 
   render() {
     return (
-      <div>
-        <link
-          rel="stylesheet"
-          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"
-        />
-        {this.renderCampaigns()}
-        <Button content="Create Campaign" icon="add circle" primary={true} />
-      </div>
+      <Layout>
+        <div>
+          <link
+            rel="stylesheet"
+            href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"
+          />
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button content="Create Campaign" icon="add circle" primary={true} />
+        </div>
+      </Layout>
     );
   }
 }
