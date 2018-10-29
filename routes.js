d763150c31fac93bd19158f0059272a904270da3
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-29 22:52:22 +0800

    Add routes for campaigns view

--- routes.js
+++ routes.js
@@ -1,4 +1,9 @@
 // It invoices the function immidiately
 const routes = require("next-routes")();
 
+/**
+ * : -> It means a wild card / parameters
+ */
+routes.add("/campaigns/:address", "/campaigns/show");
+
 module.exports = routes;
