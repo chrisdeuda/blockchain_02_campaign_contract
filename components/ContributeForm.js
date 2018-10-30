7290f54e3dcfe0570f1b673faf686ab39511f3de
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-30 17:06:32 +0800

    Add event for sending contribution to campaign

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
 
 class ContributeForm extends Component {
+  state = {
+    value: "",
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const campaign = Campaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether"),
+      });
+    } catch (error) {}
+  };
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Form.Field>
           <label>Amount of Contribute</label>
-          <Input label="ether" labelPosition="right" />
+          <Input
+            value={this.state.value}
+            onChange={event => {
+              this.setState({ value: event.target.value });
+            }}
+            label="ether"
+            labelPosition="right"
+          />
         </Form.Field>
         <Button primary>Contribute!</Button>
       </Form>
