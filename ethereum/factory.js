8e145737b3a366f1fa14d284a7f86c8a5b5f93b4
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-30 16:09:31 +0800

    Redeploy the contract with new address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,10 @@
-import web3 from './web3';
+import web3 from "./web3";
 // Need to import the ABI and the compiled contract
-import CampaignFactory from './build/CampaignFactory.json';
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    "0xBfB7DA5a918f8a454D9E82F55F052ba0BA068fc7"
+  JSON.parse(CampaignFactory.interface),
+  "0x084796B68e3e4c22e43BA5332E40e33F824e7135"
 );
 
 export default instance;
-
