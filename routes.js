7b17f6c004f2f4c42dcc9adbe4d53c5d70800539
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-31 23:22:30 +0800

    Add routes for campaign request index

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const routes = require("next-routes")();
 // It overrides the default routings of the next.js
 routes
   .add("/campaigns/new", "campaigns/new")
-  .add("/campaigns/:address", "/campaigns/show");
+  .add("/campaigns/:address", "/campaigns/show")
+  .add("/campaigns/:address/requests", "/campaigns/requests/index");
 
 module.exports = routes;
